d3907542a36d21c5c0354e73af4d98f161fd5f4d
Kyung-Tae <speed9640@naver.com>
2017-05-15 13:43:13 +0900

    취소버튼 추가함

--- app/src/main/java/com/example/kennethelee/nammuk_app/FabFoodActivity.java
+++ app/src/main/java/com/example/kennethelee/nammuk_app/FabFoodActivity.java
@@ -2,6 +2,8 @@ package com.example.kennethelee.nammuk_app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by speed on 2017-05-14.
@@ -9,9 +11,19 @@ import android.support.v7.app.AppCompatActivity;
 
 public class FabFoodActivity extends AppCompatActivity {
 
+    Button homebtn;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fab_food);
 
+        //취소버튼과 동일
+        homebtn = (Button) findViewById(R.id.button_fabfood_home);
+        homebtn.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
