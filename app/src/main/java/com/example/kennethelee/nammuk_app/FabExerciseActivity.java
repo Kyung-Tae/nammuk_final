7a496d6c79a307f966c74328608f2357b7562f92
Kennethe Lee <kevinlee230@naver.com>
2017-05-20 11:18:29 +0900

    exercise_dic 추가

--- app/src/main/java/com/example/kennethelee/nammuk_app/FabExerciseActivity.java
+++ app/src/main/java/com/example/kennethelee/nammuk_app/FabExerciseActivity.java
@@ -1,9 +1,11 @@
 package com.example.kennethelee.nammuk_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by speed on 2017-05-14.
@@ -12,6 +14,8 @@ import android.widget.Button;
 public class FabExerciseActivity extends AppCompatActivity {
 
     Button homebtn;
+    Button AerobicAddBtn;
+    Button AnaerobicAddBtn;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,32 @@ public class FabExerciseActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        AerobicAddBtn = (Button) findViewById(R.id.button_fabexercise_aerobicreg);
+        AerobicAddBtn.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //내 메뉴 만드는 레이아웃
+                Toast.makeText(FabExerciseActivity.this, "운동사전기능입니다", Toast.LENGTH_SHORT).show();
+                //내 식단 화면 보여주기
+                Intent intent = new Intent(FabExerciseActivity.this, ExerciseDicActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        AnaerobicAddBtn = (Button) findViewById(R.id.button_fabexercise_anaerobicreg);
+        AnaerobicAddBtn.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //내 메뉴 만드는 레이아웃
+                Toast.makeText(FabExerciseActivity.this, "운동사전기능입니다", Toast.LENGTH_SHORT).show();
+                //내 식단 화면 보여주기
+                Intent intent = new Intent(FabExerciseActivity.this, ExerciseDicActivity.class);
+                startActivity(intent);
+
             }
         });
 
--- app/src/main/java/com/example/kennethelee/nammuk_app/FabFoodActivity.java
+++ app/src/main/java/com/example/kennethelee/nammuk_app/FabFoodActivity.java
@@ -1,7 +1,11 @@
 package com.example.kennethelee.nammuk_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by speed on 2017-05-14.
@@ -9,9 +13,15 @@ import android.support.v7.app.AppCompatActivity;
 
 public class FabFoodActivity extends AppCompatActivity {
 
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fab_food);
+
+
+
+
 
     }
 }
