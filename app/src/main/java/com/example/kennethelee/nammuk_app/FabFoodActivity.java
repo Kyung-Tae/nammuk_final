6d2687165860e2840b6e4a9b9b029d8601e77130
Kennethe Lee <kevinlee230@naver.com>
2017-05-21 12:29:54 +0900

    myexerciseadd수정

--- app/src/main/java/com/example/kennethelee/nammuk_app/FabFoodActivity.java
+++ app/src/main/java/com/example/kennethelee/nammuk_app/FabFoodActivity.java
@@ -1,15 +1,13 @@
 package com.example.kennethelee.nammuk_app;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 /**
  * Created by speed on 2017-05-14.
  */
+
+//
 
 public class FabFoodActivity extends AppCompatActivity {
 
